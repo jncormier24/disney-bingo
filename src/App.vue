950344a4e8762c4e91745e5e6e5b587393b69dea
Joseph Cormier <jncormier24@gmail.com>
2017-10-09 18:56:43 -0400

    added database working

--- src/App.vue
+++ src/App.vue
@@ -2,13 +2,13 @@
   <div id="app">
     <div id="newEntryArea">
       <label>New Entry Text: </label>
-      <input type="text" name="newEntry" v-model="newEntry">
+      <input type="text" name="newItem" v-model="newItem.text">
       <button @click="addEntry">Add Entry</button>
     </div>
     <div id="existingEntries">
       <ul>
-        <li v-for="entry in entries">
-          {{ entry }}
+        <li v-for="entry in items">
+          {{ entry.text }}
           <span @click="removeEntry(entry)">x</span>
         </li>
       </ul>
@@ -17,21 +17,39 @@
 </template>
 
 <script>
+import firebase from 'firebase'
+
+let config = {
+  apiKey: 'AIzaSyAX5rARgskRapTBeFfEQyWFbwmYsHfnu-8',
+  authDomain: 'dsny-bingo.firebaseapp.com',
+  databaseURL: 'https://dsny-bingo.firebaseio.com',
+  projectId: 'dsny-bingo',
+  storageBucket: 'dsny-bingo.appspot.com',
+  messagingSenderId: '200961931056'
+}
+let firebaseapp = firebase.initializeApp(config)
+let db = firebaseapp.database()
+let itemsRef = db.ref('items')
 export default {
   name: 'app',
+  firebase: {
+    items: itemsRef
+  },
   data () {
     return {
-      newEntry: '',
-      entries: []
+      newItem: {
+        text: ''
+      }
     }
   },
   methods: {
     addEntry () {
-      this.entries.push(this.newEntry)
-      this.newEntry = ''
+      itemsRef.push(this.newItem)
+      this.newItem.text = ''
     },
     removeEntry (entry) {
-      this.entries.splice(this.entries.indexOf(entry), 1)
+      itemsRef.child(entry['.key']).remove()
+      // itemsRef.splice(itemsRef.indexOf(entry), 1)
     }
   }
 }
